3412a1b841646f49d2103711857b18a432b385a9
Filipengbergs <filip792@hotmail.com>
2021-12-20 13:12:46 +0100

    Skapade creatingTodoFunction()

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,6 +1,42 @@
-import { Todolist } from "./models/ITodolist";
+import { Todolist } from "./models/todolist";
 
-let newTodo: Todolist[] = [];
+let todoList: Todolist[] = [];
+
 window.onload = function () {
-    /*Write normal typescript*/
+    let userInput: HTMLInputElement = document.getElementById(
+        "user-input"
+    ) as HTMLInputElement;
+
+    let userValue = userInput.value;
+
+    creatingTodoFunction();
 };
+
+function creatingTodoFunction(): void {
+    for (let i = 0; i < todoList.length; i++) {
+        let todoContainer: HTMLDivElement = document.createElement(
+            "div"
+        ) as HTMLDivElement;
+
+        let todoTitle: HTMLSpanElement = document.createElement(
+            "span"
+        ) as HTMLSpanElement;
+
+        let finishedButton: HTMLButtonElement = document.createElement(
+            "button"
+        ) as HTMLButtonElement;
+
+        let removeButton: HTMLButtonElement = document.createElement(
+            "button"
+        ) as HTMLButtonElement;
+
+        todoTitle.innerHTML = "";
+        finishedButton.addEventListener("click", () => {});
+        removeButton.addEventListener("click", () => {});
+
+        todoContainer.appendChild(removeButton);
+        todoContainer.appendChild(finishedButton);
+        todoContainer.appendChild(todoTitle);
+        document.body.appendChild(todoContainer);
+    }
+}

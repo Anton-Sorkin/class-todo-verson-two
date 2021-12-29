21073e900466652aa5b26e7ccb8262f5420a7fd9
Anton <anton.sorkin98@outlook.com>
2021-12-29 20:21:59 +0100

    last touches

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,67 +1,86 @@
 import { Todolist } from "./models/todolist";
 
-let todoList: Todolist[] = [];
+let todoList: Todolist[] = JSON.parse(localStorage.getItem("todo")) || [];
 
 window.onload = function () {
-    let sumbitButton: HTMLButtonElement = document.querySelector("#btn");
-    sumbitButton.addEventListener("click", getUserInput);
+  let sumbitButton: HTMLButtonElement = document.querySelector("#btn");
+  sumbitButton.addEventListener("click", getUserInput);
+
+  creatingTodo();
 };
 
 function getUserInput() {
-    let input: HTMLInputElement = document.getElementById(
-        "user-input"
-    ) as HTMLInputElement;
+  let input: HTMLInputElement = document.getElementById(
+    "user-input"
+  ) as HTMLInputElement;
 
+  if (input.value.length == 0) {
+    alert("Cant add nothing as a todo, please write something");
+  } else {
     let newTodo: Todolist = new Todolist(input.value, false);
     todoList.push(newTodo);
-    console.log(input.value);
     input.value = "";
     creatingTodo();
+  }
 }
 
 function creatingTodo(): void {
-    let todoJSON: string = JSON.stringify(todoList);
-    localStorage.setItem("todo", todoJSON);
+  let todoJSON: string = JSON.stringify(todoList);
+  localStorage.setItem("todo", todoJSON);
 
-    let userInput: HTMLUListElement = document.getElementById(
-        "ul"
-    ) as HTMLUListElement;
+  let userInput: HTMLUListElement = document.getElementById(
+    "ul"
+  ) as HTMLUListElement;
 
-    userInput.innerHTML = "";
+  userInput.innerHTML = "";
 
-    for (let i = 0; i < todoList.length; i++) {
-        let todoLi: HTMLLIElement = document.createElement(
-            "li"
-        ) as HTMLLIElement;
+  for (let i = 0; i < todoList.length; i++) {
+    let todoLi: HTMLLIElement = document.createElement("li") as HTMLLIElement;
 
-        let finishedButton: HTMLButtonElement = document.createElement(
-            "button"
-        ) as HTMLButtonElement;
+    let todoText: HTMLDivElement = document.createElement(
+      "div"
+    ) as HTMLDivElement;
 
-        let removeButton: HTMLButtonElement = document.createElement(
-            "button"
-        ) as HTMLButtonElement;
+    let finishedButton: HTMLButtonElement = document.createElement(
+      "button"
+    ) as HTMLButtonElement;
 
-        todoLi.innerHTML = todoList[i].name;
-        finishedButton.addEventListener("click", () => {
-            if (todoList[i].finished == false) {
-                todoList[i].finished = true;
-                creatingTodo();
-            } else {
-                todoList[i].finished = false;
-                creatingTodo();
-            }
-        });
-        finishedButton.innerHTML = "Klar";
-        removeButton.addEventListener("click", () => {
-            todoList.splice(i, 1);
-            creatingTodo();
-        });
-        removeButton.innerHTML = "Ta bort";
+    let removeButton: HTMLButtonElement = document.createElement(
+      "button"
+    ) as HTMLButtonElement;
 
-        userInput.appendChild(todoLi);
-        userInput.appendChild(finishedButton);
-        userInput.appendChild(removeButton);
-        document.body.appendChild(userInput);
+    todoText.innerHTML = todoList[i].name;
+    todoText.className = "todoText";
+    todoLi.className = "todo";
+
+    finishedButton.innerHTML = "K";
+    finishedButton.className = "klar";
+    finishedButton.addEventListener("click", () => {
+      if (todoList[i].finished == false) {
+        todoList[i].finished = true;
+        creatingTodo();
+      } else {
+        todoList[i].finished = false;
+        creatingTodo();
+      }
+    });
+
+    removeButton.addEventListener("click", () => {
+      todoList.splice(i, 1);
+      creatingTodo();
+    });
+    removeButton.innerHTML = "R";
+    removeButton.className = "taBort";
+
+    if (todoList[i].finished) {
+      todoLi.className = "done";
+      finishedButton.innerHTML = "O";
     }
+
+    todoLi.appendChild(todoText);
+    todoLi.appendChild(finishedButton);
+    todoLi.appendChild(removeButton);
+    userInput.appendChild(todoLi);
+    document.body.appendChild(userInput);
+  }
 }
